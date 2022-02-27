b5e9f2fd299d5ac0d29ccdebf46d44b927210897
averysdad <mat.adams@live.com>
2022-02-26 20:13:53 -0500

    text

--- api/message/index.js
+++ api/message/index.js
@@ -6,8 +6,11 @@ module.exports = async function (context, req) {
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
+    //context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+     //   body: 
+    //};
+    context.res.json({
+        text: responseMessage
+    });
 }
